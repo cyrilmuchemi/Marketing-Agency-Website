6e5eeec5b79b231249755148db45c44452e5e4ce
[cyrilmuchemi] <[cyrilmuchemi@gmail.com]>
2022-08-29 15:37:17 +0300

    Create address section in the contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Address from './Address';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact</h1>
+      <div className='grid grid-cols-2 px-32'>
+      <Address/>
+      <ContactForm/>
+      </div>
     </Layout>
   )
 }
 
-export default Contact
+export default Contact;
